d8f4792cb142ab520d106e3d62b4ebae745da383
viniciusbarboosa <vindzn88@gmail.com>
2026-01-08 08:38:37 -0300

    chore: Add preload for logo image

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,6 +39,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+        <link rel="preload" as="image" href="/images/tehokaslogopngnomeelogo.png">
+
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @inertiaHead
